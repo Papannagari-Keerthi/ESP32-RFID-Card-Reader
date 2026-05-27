3ec300effe2602f860ade87a3da2535ea2e08f79
Papannagari-Keerthi <papannagarikeerthi@gmail.com>
2026-05-27 10:00:47 +0530

    Add files via upload

---
+++ sketch.ino
@@ -0,0 +1,36 @@
+#include <SPI.h>
+#include <MFRC522.h>
+
+#define SS_PIN  5
+#define RST_PIN 21
+
+MFRC522 rfid(SS_PIN, RST_PIN);
+
+void setup() {
+  Serial.begin(115200);
+  SPI.begin();
+  rfid.PCD_Init();
+  Serial.println("MFRC522 Ready");
+}
+
+void loop() {
+  delay(10);
+  if (!rfid.PICC_IsNewCardPresent()) {
+    return;
+  }
+
+  if (!rfid.PICC_ReadCardSerial()) {
+    return;
+  }
+
+  Serial.print("UID:");
+  for (byte i = 0; i < rfid.uid.size; i++) {
+    Serial.print(rfid.uid.uidByte[i] < 0x10 ? ":0" : ":");
+    Serial.print(rfid.uid.uidByte[i], HEX);
+  }
+  Serial.println();
+
+  rfid.PICC_HaltA();
+  delay(1000);
+}
+
